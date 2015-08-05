9d6cfa0c57227ad1a5303e367dd223b729b5c716
Lena <vandzelyak@gmail.com>
2015-08-06 00:02:36 +0300

    commit version 06.08.2015 00:02

--- JavaScript/lesson 1/task01.js
+++ JavaScript/lesson 1/task01.js
@@ -1,7 +1,12 @@
 /**
  * Created by Lena on 04.08.2015.
  */
+//variable declaration
 var developer;
-var name = 'Sergey';
+var name;
+//name initialization
+name = 'Sergey';
+//data copy
 developer = name;
+//display result data
 alert(developer);
---
+++ JavaScript/lesson 1/task02.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Lena on 05.08.2015.
+ */
+//variable declaration
+var name;
+//name initialization
+name = prompt ('Hi! What`s your name?', 'Leonard');
+//display result data
+alert ('Welcome, ' + name + '!');
