5c3741dde20c89f81ae4db02fb3f2e1a356de4d1
Lena <vandzelyak@gmail.com>
2015-08-10 15:43:18 +0300

    commit version 10.08.2015 15:43

---
+++ JavaScript/lesson 2/task04.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Lena on 10.08.2015.
+ */
+//variable initialization
+var a = +prompt('Enter a value of "a"', '1');
+var b = +prompt('Enter a value of "b"', '2');
+//summation of variables
+result = ((a + b >= 3) ? 'Yep!' : 'Noup!');
+alert (result);
---
+++ JavaScript/lesson 2/task05.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Lena on 10.08.2015.
+ */
+//variable initialization
+var name = prompt('Enter name: admin, manager', ''), text;
+//name validation
+text = (name == 'admin') ? 'Hi' :
+    (name == 'manager') ? 'Hello' :
+        (name == '') ? 'No login' :
+            '';
+alert(text);
+
+
