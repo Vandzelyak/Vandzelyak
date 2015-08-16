7ed1a4d9e8ea9d4d1ee67c3b7dd065f465542d9c
Lena <vandzelyak@gmail.com>
2015-08-16 22:39:50 +0300

    commit version 16.08.2015 22:40

---
+++ JavaScript/lesson 3/Codewars/convert-boolean-values-to-strings-yes-or-no.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Lena on 16.08.2015.
+ */
+/* Complete the boolToWord method.
+
+ Given: a boolean value
+
+ Return: a 'Yes' string for true and a 'No' string for false
+*/
+
+//function initialization
+ function boolToWord(bool) {
+
+     //checking if the value is boolean
+    return bool ? 'Yes' : 'No';
+}
