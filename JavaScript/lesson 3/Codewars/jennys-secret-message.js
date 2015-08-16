2a0309210d13f512bc98c9cd45b14b5ce08501ae
Lena <vandzelyak@gmail.com>
2015-08-16 22:05:38 +0300

    commit version 16.08.2015 22:05

---
+++ JavaScript/lesson 3/Codewars/jennys-secret-message.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Lena on 16.08.2015.
+ */
+
+//function initialization
+function greet(name){
+
+    //if it`s Johnny, Jenny`s greeting is special
+    //other way greeting is usual
+    return name === "Johnny" ? "Hello, my love!" : "Hello, " + name + "!";
+}
+
+//result output
+console.log (greet (name));
