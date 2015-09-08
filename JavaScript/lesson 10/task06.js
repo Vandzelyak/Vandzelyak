192e090c3ebe0f39075cfce23d05d3a9144668f5
Lena <vandzelyak@gmail.com>
2015-09-08 14:40:58 +0300

    commit version 08.09.2015 14:40

---
+++ JavaScript/lesson 10/task06.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Lena on 08.09.2015.
+ */
+
+//entering data for determining it`s type
+var input = 'vandzelyak';
+
+//creating a function to determine a type of inputted data
+function defineType(arg) {
+    return typeof arg;
+}
+
+//display results
+console.log (defineType(input));
