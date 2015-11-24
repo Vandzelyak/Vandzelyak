b62c6dafd780dbfc03a14eef32afef64b6b0fb7c
Lena <vandzelyak@gmail.com>
2015-11-24 18:59:09 +0200

    commit version 24.11.2015 18:58

--- JavaScript/Exam Project/js/main.js
+++ JavaScript/Exam Project/js/main.js
@@ -215,8 +215,15 @@ $(document).ready(function(){
 });
 
 /* flip steps on mobile */
-$(".flip-container").click(function() {
-    $(".creation").toggleClass("hovered");
+$('.creation').on("touchstart", function (e) {
+    'use strict'; //satisfy code inspectors
+    var link = $(this); //preselect the link
+    if (link.hasClass('hover')) {
+        return true;
+    } else {
+        link.addClass('hover');
+        $('.creation').not(this).removeClass('hover');
+        e.preventDefault();
+        return false; //extra, and to make sure the function has consistent return points
+    }
 });
-
-
