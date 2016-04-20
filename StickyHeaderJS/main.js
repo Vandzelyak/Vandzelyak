224f8efe5e22274a056bd3f8fc34a74555f59e1d
Lena <vandzelyak@gmail.com>
2016-04-20 15:34:15 +0300

    commit version 20.04.2016 15:34

--- StickyHeaderJS/main.js
+++ StickyHeaderJS/main.js
@@ -1,9 +1,10 @@
 /**
  * Created by Lena on 19.04.2016.
  */
+
 var headerFix = document.getElementById('nav');
 
-var headerFixTop = headerFix.getBoundingClientRect().top + window.pageYOffset;
+var headerFixTop = headerFix.getBoundingClientRect().bottom + window.pageYOffset;
 
 window.onscroll = function() {
     if (headerFix.classList.contains('fixed') && window.pageYOffset < headerFixTop) {
